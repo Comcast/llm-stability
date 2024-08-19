a4e847b75ebf3107ece0877f81208733f29556c3
Breck Baldwin <fbaldw522@cable.comcast.com>
2024-08-19 13:26:25 -0400

    added copyright info

--- main.py
+++ main.py
@@ -14,6 +14,22 @@ from helper_functions import (
 
 """Top level main function to run LLMs."""
 
+"""
+ * Copyright 2024 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+"""
+
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("--task", type=str)
 parser.add_argument("--model", type=str)
--- postprocess_responses.py
+++ postprocess_responses.py
@@ -9,6 +9,22 @@ from collections import defaultdict
 
 """Takes a llm_output.csv file from an LLM run and parses out the answer and saves as parsed_llm_output.csv"""
 
+"""
+ * Copyright 2024 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+"""
+
 
 def process_raw_response(row):
     response = row["raw_response"]
